8928edada6b0b5942a0de04fcfd96e5d13c3493e
JABALE NOOR SHAHBAJ <nurmohammadshahbaj@gmail.com>
2024-01-28 21:10:18 +0600

    mail setup

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,9 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Helper\JWTToken;
+use App\Mail\OTPMail;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class UserController extends Controller
 {
@@ -46,6 +48,29 @@ class UserController extends Controller
             ], 200);
 
         }else{
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'unauthorized'
+            ], 200);
+        }
+    }
+
+    function SendOTPCode(Request $request){
+        $email=$request->input('email');
+        $otp=rand(1000,9999);
+        $count=User::where('email','=',$email)->count();
+        if($count == 1){
+            //OTP Email Address
+            Mail::to($email)->send(new OTPMail($otp));
+            //OTP Code Table Update
+            User::where('email','=',$email)->update(['otp'=>$otp]);
+
+            return response()->json([
+                'status' => 'success',
+                'message' => '4 Digit OTP Code has been send to your email'
+            ], 200);
+        }
+        else{
             return response()->json([
                 'status' => 'failed',
                 'message' => 'unauthorized'
